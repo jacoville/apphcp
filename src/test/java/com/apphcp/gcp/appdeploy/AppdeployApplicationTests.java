8d3f9fec6a50d61711e977187e9ee6dc18d3212d
Jacob Villegas <jacobvillegas@MacBook-Pro-de-Jacob-2.local>
2020-12-11 09:02:46 -0600

    first commit

---
+++ src/test/java/com/apphcp/gcp/appdeploy/AppdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apphcp.gcp.appdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
